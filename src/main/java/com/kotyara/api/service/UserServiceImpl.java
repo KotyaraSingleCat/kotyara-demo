309096408561be40a7997f5c057974820a1cf19f
Kotyara <babanina.ev@gmail.com>
2021-03-13 01:14:31 +0200

    add back @Service annotation in UserServiceImpl.java

--- src/main/java/com/kotyara/api/service/UserServiceImpl.java
+++ src/main/java/com/kotyara/api/service/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.kotyara.api.service;
 import com.kotyara.api.entity.User;
 import com.kotyara.repository.AbstractRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Service
 public class UserServiceImpl implements AbstractService<User> {
 
   private final AbstractRepository<User> jdbcRepository;
