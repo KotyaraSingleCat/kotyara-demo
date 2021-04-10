35d9bd931b0f79c7fd96a82bd12328c50b750b1b
Kotyara <babanina.ev@gmail.com>
2021-04-10 21:57:06 +0300

    add annotation @Autowired in TicketServiceImpl.java

--- src/main/java/com/kotyara/api/abstractcrud/service/convertor/AbstractConvert.java
+++ src/main/java/com/kotyara/api/abstractcrud/service/convertor/AbstractConvert.java
@@ -1,5 +1,8 @@
 package com.kotyara.api.abstractcrud.service.convertor;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface AbstractConvert<Entity, DTO> {
 
   Entity convertToEntity(DTO dto);
--- src/main/java/com/kotyara/api/service/TicketServiceImpl.java
+++ src/main/java/com/kotyara/api/service/TicketServiceImpl.java
@@ -5,11 +5,13 @@ import com.kotyara.api.abstractcrud.service.AbstractServiceImpl;
 import com.kotyara.api.abstractcrud.service.convertor.AbstractConvert;
 import com.kotyara.api.dto.TicketDTO;
 import com.kotyara.api.entity.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TicketServiceImpl extends AbstractServiceImpl<Ticket, TicketDTO> {
 
+  @Autowired
   public TicketServiceImpl(AbstractRepository<Ticket> repository, AbstractConvert<Ticket, TicketDTO> converter) {
     super(repository, converter);
   }
