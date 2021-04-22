48aaa9809eb3b7348832fb03200dd54acbd21203
Kotyara <babanina.ev@gmail.com>
2021-04-22 16:31:08 +0300

    add pagination

--- src/main/java/com/kotyara/api/pagination/PaginationRepository.java
+++ src/main/java/com/kotyara/api/pagination/PaginationRepository.java
@@ -1,9 +1,10 @@
 package com.kotyara.api.pagination;
 
+import com.kotyara.api.entity.Ticket;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaginationRepository<Entity> extends PagingAndSortingRepository<Entity, Integer> {
+public interface PaginationRepository extends PagingAndSortingRepository<Ticket, Integer> {
 
 }
