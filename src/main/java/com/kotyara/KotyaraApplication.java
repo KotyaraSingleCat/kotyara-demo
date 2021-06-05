3e777ff7219c7a0cac4e809f8fb102fdb073b43b
KotyaraSingleCat <43034756+KotyaraSingleCat@users.noreply.github.com>
2021-06-05 14:59:29 +0300

    Update KotyaraApplication.java

--- src/main/java/com/kotyara/KotyaraApplication.java
+++ src/main/java/com/kotyara/KotyaraApplication.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAdminServer
